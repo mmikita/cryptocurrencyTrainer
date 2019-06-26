57692b1c4cfbfb35cdb86e476d6cd5a10988ceb7
Michał Mikita <paster01@gmail.com>
2019-06-26 10:56:45 +0200

    add buy/sell methods

--- app/src/main/java/com/example/cryptocurrencytrainer/MainActivity.java
+++ app/src/main/java/com/example/cryptocurrencytrainer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.cryptocurrencytrainer;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.cryptocurrencytrainer.repository.WalletRepository;
@@ -10,18 +14,32 @@ import com.example.service.CurrencyService;
 public class MainActivity extends AppCompatActivity {
 
     CurrencyService cService;
+    WalletRepository wr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((TextView)findViewById(R.id.wallet)).setText(WalletRepository.getCurrentWallet());
+        wr = new WalletRepository(this);
+
+        ((TextView)findViewById(R.id.wallet)).setText(wr.getCurrentWallet());
         cService = new CurrencyService();
         ((TextView)findViewById(R.id.btc)).setText(cService.getCurrentBitcoinRate());
         ((TextView)findViewById(R.id.etm)).setText(cService.getCurrenteternumRate());
         ((TextView)findViewById(R.id.ltc)).setText(cService.getLitecoinRate());
 
+
+    }
+
+    public void buy(View view) {
+wr.setValue();
+        ((TextView)findViewById(R.id.wallet)).setText(wr.getCurrentWallet());
+    }
+
+    public void sell(View view) {
+        wr.setValue();
+        ((TextView)findViewById(R.id.wallet)).setText(wr.getCurrentWallet());
 
     }
 }
--- app/src/main/java/com/example/cryptocurrencytrainer/repository/WalletRepository.java
+++ app/src/main/java/com/example/cryptocurrencytrainer/repository/WalletRepository.java
@@ -1,9 +1,59 @@
 package com.example.cryptocurrencytrainer.repository;
 
-public class WalletRepository {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-    public static String getCurrentWallet(){
+public class WalletRepository extends SQLiteOpenHelper {
 
-        return "5000 zł";
+
+    private static final String DB_NAME = "coffeina";
+    private static final int DB_VERSION = 1;
+    SQLiteDatabase db;
+
+    public WalletRepository(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        db = this.getReadableDatabase();
+    }
+
+    public  String getCurrentWallet(){
+
+
+
+        Cursor cursor = db.query ("WALLET",
+                new String[] {"VALUE"},
+                "_id = ?",
+                new String[] {Integer.toString(1)},
+                null, null,null);
+
+
+        return cursor.moveToFirst()? cursor.getString(0)+ " zł": "0 zł";
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE WALLET(_id INTEGER PRIMARY KEY AUTOINCREMENT, VALUE TEXT)");
+
+
+        ContentValues walletRow = new ContentValues();
+        walletRow.put("VALUE", "323");
+        db.insert("WALLET", null, walletRow);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void setValue()
+    {
+        ContentValues walletRow = new ContentValues();
+        walletRow.put("VALUE", "1111");
+        db.update("WALLET", walletRow, "_id = ?", new String[]{"1"});
     }
 }
