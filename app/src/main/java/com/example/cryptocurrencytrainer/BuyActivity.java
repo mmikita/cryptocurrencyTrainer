99b2dadd9748d1f6d36fa3d85cafcbe974edfde9
Michał Mikita <paster01@gmail.com>
2019-06-26 20:51:28 +0200

    add edittext and spinner listener

--- app/src/main/java/com/example/cryptocurrencytrainer/BuyActivity.java
+++ app/src/main/java/com/example/cryptocurrencytrainer/BuyActivity.java
@@ -2,22 +2,70 @@ package com.example.cryptocurrencytrainer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.cryptocurrencytrainer.service.CurrencyService;
 
 public class BuyActivity extends AppCompatActivity {
+    CurrencyService service;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
+        service = new CurrencyService();
        Spinner spinner = findViewById(R.id.coins);
         TextView cost = findViewById(R.id.cost);
       //  TextView selectedCoin = (TextView)spinner.getSelectedView(); selectedCoin.getText().toString();
-        cost.setText(CurrencyService.getCurrentCoinCost("Bitcoin"));
+        cost.setText(service.getCurrentCoinCost("Bitcoin"));
+        EditText quantity = (EditText) findViewById(R.id.quantity);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
+        quantity.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+
+            }
+        });
 
 
+    }
+
+
+    public void buyCoins(){
+        TextView fullCost = findViewById(R.id.fullCost);
+        EditText quantity = (EditText) findViewById(R.id.quantity);
+
+
+        String message = quantity.getText().toString();
+        fullCost.setText(service.calculateFullCost(fullCost.getText().toString(), ""));
     }
 }
--- app/src/main/java/com/example/cryptocurrencytrainer/service/CurrencyService.java
+++ app/src/main/java/com/example/cryptocurrencytrainer/service/CurrencyService.java
@@ -5,16 +5,25 @@ import com.example.cryptocurrencytrainer.api.CurrencyExchangeCall;
 
 public class CurrencyService {
 
-    public static String getCurrentCoinCost(String type) {
+    public  String getCurrentCoinCost(String type) {
         String costInUSD = CryptoCoinsCall.getCoinCost(type);
         double costInPLN = Double.parseDouble(costInUSD) * Double.parseDouble(CurrencyExchangeCall.getRatting(type));
         costInPLN *= 100;
         costInPLN = Math.round(costInPLN);
-        costInPLN /= 100; 
+        costInPLN /= 100;
         String costInString = String.valueOf(costInPLN);
 
         return costInString;
     }
 
+    public String calculateFullCost(String cost, String qunatity){
+
+        double fullCost = Double.parseDouble(cost)*Double.parseDouble(qunatity);
+        fullCost *= 100;
+        fullCost = Math.round(fullCost);
+        fullCost /= 100;
+
+        return String.valueOf(fullCost);
+    }
 
 }
