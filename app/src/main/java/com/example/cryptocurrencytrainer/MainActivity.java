8421629f0aa38f7c387ba1e072c246900dbcfbd6
Michał Mikita <paster01@gmail.com>
2019-06-26 09:35:35 +0200

    layout changes

--- app/src/main/java/com/example/cryptocurrencytrainer/MainActivity.java
+++ app/src/main/java/com/example/cryptocurrencytrainer/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.cryptocurrencytrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.cryptocurrencytrainer.repository.WalletRepository;
+import com.example.service.CurrencyService;
 
 public class MainActivity extends AppCompatActivity {
+
+    CurrencyService cService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView)findViewById(R.id.wallet)).setText(WalletRepository.getCurrentWallet());
+        cService = new CurrencyService();
+        ((TextView)findViewById(R.id.btc)).setText(cService.getCurrentBitcoinRate());
+        ((TextView)findViewById(R.id.etm)).setText(cService.getCurrenteternumRate());
+        ((TextView)findViewById(R.id.ltc)).setText(cService.getLitecoinRate());
+
+
     }
 }
---
+++ app/src/main/java/com/example/cryptocurrencytrainer/repository/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.example.cryptocurrencytrainer.repository;
+
+public class WalletRepository {
+
+    public static String getCurrentWallet(){
+
+        return "5000 zł";
+    }
+}
